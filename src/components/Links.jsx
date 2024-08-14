0ea76cda290474305aec3df24e9b42fdce786d6f
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-14 17:34:27 -0500

    Added links to profiles and email.

---
+++ src/components/Links.jsx
@@ -0,0 +1,12 @@
+export default function Links() {
+    return (
+        <div className='links'>
+            <h2>
+                My profiles:
+            </h2>
+            <a className='linkOne' href='https://github.com/JustindHolderman' target='_blank' rel='noreferrer'>Github</a>
+            <a className='linkTwo' href='https://www.linkedin.com/in/justin-holderman-522228321/' target='_blank' rel='noreferrer'>LinkedIn</a>
+             <a className="link-a" href='justindholderman@gmail.com'>Email</a>
+        </div>
+    )
+}
