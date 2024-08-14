823d3c2044461142fb9f79c59554d39ec825fe3f
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-14 17:41:38 -0500

    Added preliminary About section.

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+
+    return (
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <div className="about__content">
+                <p>
+                    I am a full stack web developer with a background in the arts. I have a passion for creating and designing. I am always looking for new ways to challenge myself and grow as a developer. I have experience in HTML, CSS, JavaScript, Node.js, Express.js, React.js, and MongoDB. I am always looking for new opportunities to learn and grow as a developer.
+                </p>
+            </div>
+        </section>
+    )
+}
