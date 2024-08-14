792da9443710a983b850623a9e725929ab791b1f
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-14 17:23:11 -0500

    Added Portfolio applications.

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,23 @@
+export default function Portfolio() {
+
+    const applications = [
+        {
+            name: "Text Editor PWA",
+            url: "https://text-editor-w8n2.onrender.com/",
+            description: "A simple text editor PWA built with React.",
+            repo: "https://github.com/JustindHolderman/Text_Editor"
+        },
+        {
+            name: "Weather Dashboard",
+            url: "https://justindholderman.github.io/weather-dashboard/",
+            description: "A weather dashboard utilizing server side API.",
+            repo: "https://github.com/JustindHolderman/weather-dashboard",
+        },
+        {
+            name: "Kanban Board",
+            url: "https://justindholderman.github.io/kanban/",
+            description: "A kanban board for task management.",
+            repo: "https://github.com/JustindHolderman/kanban",
+        },
+    ]
+}
