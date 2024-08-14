84a19dacd61a99fb4e5d6773c5cf5442683b98a5
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-14 17:10:04 -0500

    Imported and created routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+import './css/index.css';
+import './css/normalize.css';
+import './css/background.css';
+
+import App from './App.jsx';
+import Homepage from './pages/HomePage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+import ErrorHandle from './pages/Error.jsx';
+import ResumePage from './pages/ResumePage';
+
+
+const routes = [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorHandle />,
+      children: [
+        {
+          index: true,
+          element: <Homepage />
+        },
+        {
+          path: 'contact',
+          element: <ContactPage />
+        },
+        {
+          path: 'portfolio',
+          element: <PortfolioPage />
+        },
+        {
+          path: 'resume',
+          element: <ResumePage />
+        }
+      ]
+    }
+  ];
+
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={createBrowserRouter()}>
+      <App />
+    </RouterProvider>
+  );
